abf2f951cca301f75c9cc4b1f656c0ea357681a5
Mark Willett <markwillett92@gmail.com>
2016-09-13 17:45:12 +0100

    Initial upload of hello world example

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -10,9 +10,31 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) IBOutlet UILabel *messageLabel;
+@property (nonatomic, strong) IBOutlet UITextField *inputField;
+
 @end
 
 @implementation ViewController
+
+-(IBAction)enter {
+    NSString *yourName = self.inputField.text;
+    NSString *myName = @"Mark";
+    NSString *message;
+    
+    if([yourName length] == 0){
+        yourName = @"World";
+    }
+    
+    if([yourName isEqualToString:myName]){
+        message = [NSString stringWithFormat:@"Hello %@, we have the same name!", yourName];
+    } else {
+        message = [NSString stringWithFormat:@"Hello %@", yourName];
+    }
+    
+    self.messageLabel.text = message;
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
